61950a22ec08de631cb9652283164bf5e7adcfe7
vikavolovik03-web <vika.volovik.03@mail.com>
2026-04-15 00:59:04 +0300

    Prepare for Render deploy

--- app.py
+++ app.py
@@ -439,4 +439,6 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5001)
+    
+port = int(os.environ.get('PORT', 5001))
+app.run(debug=False, host='0.0.0.0', port=port)
